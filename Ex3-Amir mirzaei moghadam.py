46c69e2cde67f2d18e4fb990215b08963e2792f8
amirmirzaeimoghadam <68754620+amirmirzaeimoghadam@users.noreply.github.com>
2020-07-24 08:35:34 -1100

    Add files via upload

---
+++ Ex3-Amir mirzaei moghadam.py
@@ -0,0 +1,19 @@
+year = int (input ("plz entsr your year="))
+
+if  year % 4 != 0 :
+    print("your year is not kabisse")
+
+else :
+     if year % 400  == 0 :
+        print("your year is kabisse")
+    
+     elif year % 100 == 0 :
+        print("your year is not kabisse")
+
+
+     else  :
+          print("your year is kabisse")
+         
+
+      
+   
