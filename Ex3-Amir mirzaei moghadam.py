692a4f0ea4763b6432eaf32eec28b43a76f3ae3f
amirmirzaeimoghadam <68754620+amirmirzaeimoghadam@users.noreply.github.com>
2020-07-24 21:21:47 -1100

    Add files via upload

--- Ex3-Amir mirzaei moghadam.py
+++ Ex3-Amir mirzaei moghadam.py
@@ -1,11 +1,16 @@
 year = int (input ("plz entsr your year="))
+month = int (input ("plz entsr your month="))
+day = int (input ("plz entsr your day="))
+kabiise = 0
 
 if  year % 4 != 0 :
     print("your year is not kabisse")
+    
 
 else :
      if year % 400  == 0 :
         print("your year is kabisse")
+        kabiise = 1
     
      elif year % 100 == 0 :
         print("your year is not kabisse")
@@ -13,7 +18,16 @@ else :
 
      else  :
           print("your year is kabisse")
+          kabiise = 1
          
 
       
    
+list_month = [31 , 28 ,31 , 30 , 31 ,30 , 31, 31 ,30 , 31, 30 , 31]
+
+if kabiise == 1:
+    list_month[1] =  29
+
+    
+ruz = sum(list_month[:month -1]) + day
+print (ruz , 'omin ruze sal')
